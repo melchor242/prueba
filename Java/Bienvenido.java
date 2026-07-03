471a7c8cbc2982604c089309d27ec6d6d9baeed0
melchor242 <omar.is.valdezm@gmail.com>
2026-07-03 13:01:42 -0600

    prueba de universidad

---
+++ Java/Bienvenido.java
@@ -0,0 +1,22 @@
+public class Bienvenido {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   BIENVENIDO AL MUNDO DE JAVA   ");
+        System.out.println("=================================\n");
+
+        String nombre = "Programador";
+
+        System.out.println("Hola, " + nombre + "!");
+        System.out.println("Este es tu primer programa crack");
+
+        int numeroFavorito = 42;
+
+        System.out.println("\nDato curioso:");
+        System.out.println("Tu número favorito es " + numeroFavorito +
+                           " y su doble es " + (numeroFavorito * 2));
+
+        System.out.println("\n¡Que comience la aventura en Java!");
+    }
+}
